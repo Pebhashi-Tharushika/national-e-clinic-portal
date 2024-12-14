6cd81d4fe7e1d73075906deb2bf9d63d32ae2961
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2024-12-14 19:02:29 +0530

    initial commit

---
+++ header.php
@@ -0,0 +1,32 @@
+<?php 
+session_start();
+?>
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>National E Clinic Portal</title>
+    <link rel="icon" href="images/logo_v.png" type="image/png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+    <link rel="stylesheet" href="style/heder.css">
+    <script src="https://kit.fontawesome.com/5a39afcc99.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    
+
+  <section class="container-wrapper">
+    <div class="container">
+      <header>
+        <div class="logo"><img src="images/logo_h.png" alt="logo"/></div>
+        <div class="nav">
+          <a class="active" href="home/home.php">Home</a>
+          <a class="active" href="about_us/about_us.php">About Us</a>
+          <a class="active" href="account/account.php">Account</a>
+        </div>
+      </header>
+    </div>
+        </section>
+
