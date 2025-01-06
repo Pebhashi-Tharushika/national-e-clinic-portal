69b5b5a7ce466bf65fcd91a3c5b2fbe934a3ff63
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2025-01-06 12:18:31 +0530

    complete services page

--- index.php
+++ index.php
@@ -33,6 +33,7 @@ session_start();
   <link rel="stylesheet" href="/national-e-clinic-portal/style/footer.css">
   <link rel="stylesheet" href="/national-e-clinic-portal/style/home.css">
   <link rel="stylesheet" href="/national-e-clinic-portal/style/about-us.css">
+  <link rel="stylesheet" href="/national-e-clinic-portal/style/services.css">
 
   <!-- bootstrap -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
---
+++ services.php
@@ -0,0 +1,212 @@
+<section id="services-container-wrapper">
+    <div id="services-container">
+        <h2 id="services-title">Services We Offer</h2>
+
+        <div class="grid text-center" id="service-content">
+            <div class="g-col-3 service-item">
+                <a href="">
+                    <div class="inner-service-item">
+                        <div class="service-image-container"><img src="/national-e-clinic-portal/images/services/dental.jpeg" alt="service-img"></div>
+                        <h3>Dental Clinics</h3>
+                    </div>
+                </a>
+            </div>
+            <div class="g-col-3 service-item">
+                <a href="">
+                    <div class="inner-service-item">
+                        <div class="service-image-container"><img src="/national-e-clinic-portal/images/services/dermatology.jpg" alt="service-img"></div>
+                        <h3>Dermatology Clinics</h3>
+                    </div>
+                </a>
+            </div>  
+            <div class="g-col-3 service-item">
+                <a href="">
+                    <div class="inner-service-item">
+                        <div class="service-image-container"><img src="/national-e-clinic-portal/images/services/maternity.jpg" alt="service-img"></div>
+                        <h3>Maternity and Baby Clinics</h3>
+                    </div>
+                </a>
+            </div>
+            <div class="g-col-3 service-item">
+                <a href="">
+                    <div class="inner-service-item">
+                        <div class="service-image-container"><img src="/national-e-clinic-portal/images/services/pediatric.png" alt="service-img"></div>
+                        <h3>Pediatric Clinics</h3>
+                    </div>
+                </a>
+            </div> 
+
+            
+            <div class="g-col-3 service-item">
+                <a href="">
+                    <div class="inner-service-item">
+                        <div class="service-image-container"><img src="/national-e-clinic-portal/images/services/orthopedic.jpg" alt="service-img"></div>
+                        <h3>Orthopedic Clinics</h3>
+                    </div>
+                </a>
+            </div> 
+            <div class="g-col-3 service-item">
+                <a href="">
+                    <div class="inner-service-item">
+                        <div class="service-image-container"><img src="/national-e-clinic-portal/images/services/cardiology.jpg" alt="service-img"></div>
+                        <h3>Cardiology Clinics</h3>
+                    </div>
+                </a>
+            </div> 
+            <div class="g-col-3 service-item">
+                <a href="">
+                    <div class="inner-service-item">
+                        <div class="service-image-container"><img src="/national-e-clinic-portal/images/services/ophthalmology.jpg" alt="service-img"></div>
+                        <h3>Ophthalmology Clinics</h3>
+                    </div>
+                </a>
+            </div>
+            <div class="g-col-3 service-item">
+                <a href="">
+                    <div class="inner-service-item">
+                        <div class="service-image-container"><img src="/national-e-clinic-portal/images/services/oncology.jpg" alt="service-img"></div>
+                        <h3>Oncology Clinics</h3>
+                    </div>
+                </a>
+            </div> 
+
+            <div class="g-col-3 service-item">
+                <a href="">
+                    <div class="inner-service-item">
+                        <div class="service-image-container"><img src="/national-e-clinic-portal/images/services/neurology.png" alt="service-img"></div>
+                        <h3>Neurology Clinics</h3>
+                    </div>
+                </a>
+            </div> 
+            <div class="g-col-3 service-item">
+                <a href="">
+                    <div class="inner-service-item">
+                        <div class="service-image-container"><img src="/national-e-clinic-portal/images/services/endocrinology.jpg" alt="service-img"></div>
+                        <h3>Endocrinology Clinics</h3>
+                    </div>
+                </a>
+            </div>
+            <div class="g-col-3 service-item">
+                <a href="">
+                    <div class="inner-service-item">
+                        <div class="service-image-container"><img src="/national-e-clinic-portal/images/services/gastroenterology.png" alt="service-img"></div>
+                        <h3>Gastroenterology Clinics</h3>
+                    </div>
+                </a>
+            </div> 
+            <div class="g-col-3 service-item">
+                <a href="">
+                    <div class="inner-service-item">
+                        <div class="service-image-container"><img src="/national-e-clinic-portal/images/services/urology.png" alt="service-img"></div>
+                        <h3>Urology and Renal Clinics</h3>
+                    </div>
+                </a>
+            </div>
+
+            <div class="g-col-3 service-item">
+                <a href="">
+                    <div class="inner-service-item">
+                        <div class="service-image-container"><img src="/national-e-clinic-portal/images/services/pulmonology.png" alt="service-img"></div>
+                        <h3>Pulmonology Clinics</h3>
+                    </div>
+                </a>
+            </div> 
+            <div class="g-col-3 service-item">
+                <a href="">
+                    <div class="inner-service-item">
+                        <div class="service-image-container"><img src="/national-e-clinic-portal/images/services/rheumatology.jpg" alt="service-img"></div>
+                        <h3>Rheumatology Clinics</h3>
+                    </div>
+                </a>
+            </div>
+            <div class="g-col-3 service-item">
+                <a href="">
+                    <div class="inner-service-item">
+                        <div class="service-image-container"><img src="/national-e-clinic-portal/images/services/infectious-diseases.jpg" alt="service-img"></div>
+                        <h3>Infectious Diseases Clinics</h3>
+                    </div>
+                </a>
+            </div>
+            <div class="g-col-3 service-item">
+                <a href="">
+                    <div class="inner-service-item">
+                        <div class="service-image-container"><img src="/national-e-clinic-portal/images/services/hematology.jpg" alt="service-img"></div>
+                        <h3>Hematology Clinics</h3>
+                    </div>
+                </a>
+            </div>
+
+            
+            <div class="g-col-3 service-item">
+                <a href="">
+                    <div class="inner-service-item">
+                        <div class="service-image-container"><img src="/national-e-clinic-portal/images/services/nutrition-and-dietetics.jpg" alt="service-img"></div>
+                        <h3>Nutrition and Dietetics Clinics</h3>
+                    </div>
+                </a>
+            </div>
+            <div class="g-col-3 service-item">
+                <a href="">
+                    <div class="inner-service-item">
+                        <div class="service-image-container"><img src="/national-e-clinic-portal/images/services/chiropractic.jpeg" alt="service-img"></div>
+                        <h3>Chiropractic Clinics</h3>
+                    </div>
+                </a>
+            </div>
+            <div class="g-col-3 service-item">
+                <a href="">
+                    <div class="inner-service-item">
+                        <div class="service-image-container"><img src="/national-e-clinic-portal/images/services/vascular.jpg" alt="service-img"></div>
+                        <h3>Vascular Clinics</h3>
+                    </div>
+                </a>
+            </div>
+            <div class="g-col-3 service-item">
+                <a href="">
+                    <div class="inner-service-item">
+                        <div class="service-image-container"><img src="/national-e-clinic-portal/images/services/rehabilitation.jpg" alt="service-img"></div>
+                        <h3>Rehabilitation Clinics</h3>
+                    </div>
+                </a>
+            </div>
+
+            <div class="g-col-3 service-item">
+                <a href="">
+                    <div class="inner-service-item">
+                        <div class="service-image-container"><img src="/national-e-clinic-portal/images/services/allergy.png" alt="service-img"></div>
+                        <h3>Allergy and Immunology, Cell Biology Clinics</h3>
+                    </div>
+                </a>
+            </div>
+            <div class="g-col-3 service-item">
+                <a href="">
+                    <div class="inner-service-item">
+                        <div class="service-image-container"><img src="/national-e-clinic-portal/images/services/psychology.jpg" alt="service-img"></div>
+                        <h3>Psychology and Mental Health Clinics</h3>
+                    </div>
+                </a>
+            </div>
+            <div class="g-col-3 service-item">
+                <a href="">
+                    <div class="inner-service-item">
+                        <div class="service-image-container"><img src="/national-e-clinic-portal/images/services/ent.png" alt="service-img"></div>
+                        <h3>ENT (Ear, Nose and Throat) Clinics</h3>
+                    </div>
+                </a>
+            </div> 
+            <div class="g-col-3 service-item">
+                <a href="">
+                    <div class="inner-service-item">
+                        <div class="service-image-container"><img src="/national-e-clinic-portal/images/services/general-and-family.jpg" alt="service-img"></div>
+                        <h3>General and Family Medicine Clinics</h3>
+                    </div>
+                </a>
+            </div>
+            
+        </div>
+
+
+    </div>
+
+
+</section>
