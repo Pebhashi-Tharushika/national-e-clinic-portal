cebe2788a42eaf497534012b95b060da12f4ad75
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2025-01-12 13:23:24 +0530

    design clinic details page

---
+++ see-clinic-details.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Select a Province</title>
+    <link rel="stylesheet" href="/national-e-clinic-portal/style/see-clinic-details.css">
+</head>
+<body>
+    <div class="container">
+        <h1 class="title">Select a Province</h1>
+        <div class="grid-container">
+
+            <a onclick="window.location.href='../clinic_details/provinces/central_province.php'" class="grid-item" id="province1">Central</a>
+
+            <a onclick="window.location.href='../clinic_details/provinces/eastern_province.php'" class="grid-item" id="province2">Eastern</a>
+
+            <a onclick="window.location.href='../clinic_details/provinces/nothern_province.php'" class="grid-item" id="province3">Northern</a>
+
+            <a onclick="window.location.href='../clinic_details/provinces/north_central_province.php'" class="grid-item" id="province4">North Central</a>
+
+            <a onclick="window.location.href='../clinic_details/provinces/sabaragamuwa_province.php'" class="grid-item" id="province5">Sabaragamuwa</a>
+
+            <a onclick="window.location.href='../clinic_details/provinces/north_western_province.php'" class="grid-item" id="province6">North Western</a>
+
+            <a onclick="window.location.href='../clinic_details/provinces/southern_province.php'"class="grid-item" id="province7">Southern</a>
+
+            <a onclick="window.location.href='../clinic_details/provinces/uva_province.php'" class="grid-item" id="province8">Uva</a>
+
+            <a onclick="window.location.href='../clinic_details/provinces/western_province.php'" class="grid-item" id="province9">Western</a>
+        </div>
+    </div>
+</body>
+</html>
